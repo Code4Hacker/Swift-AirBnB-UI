b6ed0af65c622fe4d871a60623d1b0455a94714b
Code4Hacker <black2022mail@gmail.com>
2024-03-11 09:50:57 +0300

    Initial Commit

---
+++ Session2/Session2App.swift
@@ -0,0 +1,17 @@
+//
+//  Session2App.swift
+//  Session2
+//
+//  Created by Gemini Child on 11/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Session2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
